9ec362cfd37da98bc06c5052da3f69d6003ed5d2
MleMoe <i@mle.moe>
2021-11-15 23:58:26 +0800

    :tada: init project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,48 @@
+import "./public-path";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// new Vue({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).$mount("#app");
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+let instance: any = null;
+function render(props: any = {}) {
+  const { container } = props;
+
+  instance = new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount(container ? container.querySelector("#app") : "#app");
+}
+
+// 独立运行时
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped");
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function mount(props: any) {
+  console.log("[vue] props from main framework", props);
+  render(props);
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+  // router = null;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,18 +6,26 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
+    path: (window as any).__POWERED_BY_QIANKUN__ ? "/app-vue2/" : "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    children: [
+      // 其它的路由都写到这里
+      {
+        path: "/",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      },
+    ],
   },
 ];
 
