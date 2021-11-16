25e2d73dc0240871c3cdbc0cb85c60ec18159e4a
MleMoe <i@mle.moe>
2021-11-16 21:27:39 +0800

    :bug: fix router match

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,42 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
+const prefix = (window as any).__POWERED_BY_QIANKUN__
+  ? process.env.VUE_APP_MICRO_ROUTER
+  : '';
+
 const routes: Array<RouteConfig> = [
   {
-    path: (window as any).__POWERED_BY_QIANKUN__
-      ? process.env.VUE_APP_MICRO_ROUTER
-      : '/',
+    path: prefix + '/',
     name: 'Home',
     component: Home,
-    children: [
-      // 其它的路由都写到这里
-      {
-        path: '/',
-        name: 'Home',
-        component: Home,
-      },
-      {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-          import(/* webpackChunkName: "about" */ '../views/About.vue'),
-      },
-    ],
+  },
+  {
+    path: prefix + '/about',
+    name: 'About',
+    component: About,
   },
 ];
 
 const router = new VueRouter({
   routes,
 });
+
+// 判断 qiankun 环境则进行路由拦截，判断跳转路由是否有 /micro 开头前缀，没有则加上
+if ((window as any).__POWERED_BY_QIANKUN__) {
+  router.beforeEach((to, from, next) => {
+    if (!to.path.includes(prefix)) {
+      next({
+        path: prefix + to.path,
+      });
+    } else {
+      next();
+    }
+  });
+}
 
 export default router;
