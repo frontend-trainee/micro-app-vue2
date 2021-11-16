eac90b54f5f174038a5444cc3a03f8e3215fe2d0
MleMoe <i@mle.moe>
2021-11-16 22:09:00 +0800

    :sparkles: unify #repo-name as route base

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const prefix = (window as any).__POWERED_BY_QIANKUN__
   ? process.env.VUE_APP_MICRO_ROUTER
-  : '';
+  : "";
 
 const routes: Array<RouteConfig> = [
   {
-    path: prefix + '/',
-    name: 'Home',
+    path: prefix + "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: prefix + '/about',
-    name: 'About',
+    path: prefix + "/about",
+    name: "About",
     component: About,
   },
 ];
@@ -26,7 +26,7 @@ const router = new VueRouter({
   routes,
 });
 
-// 判断 qiankun 环境则进行路由拦截，判断跳转路由是否有 /micro 开头前缀，没有则加上
+// 判断 qiankun 环境则进行路由拦截，判断跳转路由是否有 micro 应用开头前缀，没有则加上
 if ((window as any).__POWERED_BY_QIANKUN__) {
   router.beforeEach((to, from, next) => {
     if (!to.path.includes(prefix)) {
