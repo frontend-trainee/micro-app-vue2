87b742a399e9c52b44658f216cbf1932883c89ae
MleMoe <i@mle.moe>
2021-11-16 17:40:24 +0800

    :pencil2: fix typos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,31 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: (window as any).__POWERED_BY_QIANKUN__
-      ? process.env.VUE_APP_MiCRO_ROUTER
-      : "/",
-    name: "Home",
+      ? process.env.VUE_APP_MICRO_ROUTER
+      : '/',
+    name: 'Home',
     component: Home,
     children: [
       // 其它的路由都写到这里
       {
-        path: "/",
-        name: "Home",
+        path: '/',
+        name: 'Home',
         component: Home,
       },
       {
-        path: "/about",
-        name: "About",
+        path: '/about',
+        name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-          import(/* webpackChunkName: "about" */ "../views/About.vue"),
+          import(/* webpackChunkName: "about" */ '../views/About.vue'),
       },
     ],
   },
